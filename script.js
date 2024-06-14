b9099ee85f9aff40db8c6dfdb7a03e028dfe91d8
Shouzi <shouziadeeb123@gmail.com>
2024-06-14 18:55:16 +0530

    added weather folder to my repo

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("check");
+
+const input = document.querySelector(".searchBar input");
+const searchButton = document.querySelector(".searchBar button");
+const temp = document.querySelector(".temp");
+const feel = document.querySelector(".feel");
+const city = document.querySelector(".city");
+const BackgrounVideo = document.querySelector(".Background_video");
+const humidity = document.querySelector(".humidity");
+const windspeed = document.querySelector(".wind");
+const weather_icon = document.querySelector(".weather-icon");
+const apikeycode = "9b382d893be25a57125186ae185c474f";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+window.onload = () => {
+  checkWeather("amroha");
+};
+
+async function checkWeather(cityName) {
+  const response = await fetch(apiUrl + cityName + `&appid=${apikeycode}  `);
+  var data = await response.json();
+  console.log(data);
+  if (data.cod == 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".invalid_mssg").style.display = "block";
+    BackgrounVideo.src= "";
+    return;
+  }
+  city.innerHTML = data.name;
+  feel.innerHTML = "feels like " + Math.round(data.main.feels_like) + "°c";
+  temp.innerHTML = Math.round(data.main.temp) + "°c";
+  humidity.innerHTML = data.main.humidity + "%";
+  windspeed.innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weather_icon.src = "cloudsun.png";
+    BackgrounVideo.src = "istockphoto-1355532687-640_adpp_is.mp4";
+  }
+  if (data.weather[0].main == "Haze") {
+    weather_icon.src = "clearcloud.png";
+    BackgrounVideo.src = "hazevideo.mp4";
+  }
+  if (data.weather[0].main == "Rain") {
+    weather_icon.src = "rain.png";
+    BackgrounVideo.src = "rainvideo.mp4";
+  }
+  if (data.weather[0].main == "Clear") {
+    weather_icon.src = "sun.png";
+    BackgrounVideo.src = "clearWeather.mp4";
+  }
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".invalid_mssg").style.display = "none";
+}
+searchButton.addEventListener("click", () => {
+  checkWeather(input.value);
+});
